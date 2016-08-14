220a6f8ac03a2db24155e6f6687bee7b011c4816
misu <parkour12019@gmail.com>
2016-08-14 17:02:18 +0900

    first commit

---
+++ main.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+    "net/http"
+    "github.com/labstack/echo"
+    "github.com/labstack/echo/engine/standard"
+)
+
+type User struct {
+    Name  string `json:"name"`
+}
+
+
+
+
+func main() {
+
+
+    e := echo.New()
+    e.GET("/", func(c echo.Context) error {
+        user := User{Name: "misu"}
+        return c.JSON(http.StatusCreated, user)
+    })
+
+    e.Run(standard.New(":5000"))
+}
